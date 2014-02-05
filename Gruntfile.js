4b49d9e5078fe7b632f8cde30a79b21495e03163
Ben Alman <cowboy@rj3.net>
2014-02-05 13:57:14 -0500

    Update to Grunt 0.4.x. Closes gh-3.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,62 +2,81 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: '<json:package.json>',
-    meta: {
-      name: 'JavaScript Hooker',
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! JavaScript Hooker - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    clean: {
+      dist: 'dist',
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>\n',
+        stripBanners: true,
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/ba-<%= pkg.name %>.js',
+      },
     },
     uglify: {
       options: {
-        banner: '/*\n' +
-                ' * Javascript Hooker\n' +
-                ' * http://github.com/cowboy/javascript-hooker\n' +
-                ' * Copyright (c) 2012 "Cowboy" Ben Alman\n' +
-                ' * Licensed under the MIT license.\n' +
-                ' */\n'
+        banner: '<%= banner %>',
       },
       dist: {
-        files: {
-          'dist/ba-hooker.min.js': 'dist/ba-hooker.js'
-        }
+        expand: true,
+        src: '<%= concat.dist.dest %>',
+        ext: '.min.js',
+        // dest: 'dist/<%= pkg.name %>.min.js',
       }
     },
     nodeunit: {
-      files: ['test/**/*.js']
-    },
-    lint: {
-      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint:files nodeunit:files'
+      files: ['test/**/*_test.js']
     },
     jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true
+      build: {
+        options: {jshintrc: '.jshintrc'},
+        src: ['Gruntfile.js'],
       },
-      globals: {
-        exports: true,
-        module: false
-      }
+      lib: {
+        options: {jshintrc: 'lib/.jshintrc'},
+        src: ['lib/**/*.js'],
+      },
+      test: {
+        options: {jshintrc: 'lib/.jshintrc'},
+        src: ['test/**/*.js'],
+      },
+    },
+    watch: {
+      build: {
+        files: '<%= jshint.build.src %>',
+        tasks: ['jshint:build']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'nodeunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      },
     },
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
-  grunt.registerTask('default', ['lint nodeunit uglify']);
-  grunt.registerTask('test', ['lint nodeunit']);
+  grunt.registerTask('default', ['test', 'build']);
+
+  grunt.registerTask('test', ['jshint', 'nodeunit']);
+  grunt.registerTask('build', ['clean', 'concat', 'uglify']);
 
 };
